869ea3fbd7e530a578bd017eae737d18b665d2ab
guto <sergio_dognini@yahoo.com.br>
2019-09-17 23:57:57 -0300

    novo fundo

--- src/components/FormLogin.js
+++ src/components/FormLogin.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, TextInput, Button, TouchableHighlight, ImageBackground, ActivityIndicator } from 'react-native';
+import { View, Text, TextInput, StyleSheet, TouchableHighlight,TouchableOpacity, ImageBackground, ActivityIndicator } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import { connect } from 'react-redux';
 import { modificaEmail, modificaSenha, autenticarUsuario } from '../actions/AutenticacaoActions';
@@ -17,26 +17,28 @@ class formLogin extends Component {
       );
     }
     return (
-      <Button 
-        title='Acessar' 
-        color='#000' 
-        onPress={() => this._autenticarUsuario()}
-      />
+      <TouchableOpacity 
+      onPress={() => this._autenticarUsuario()} 
+      style={[styles.button]}>
+          <Text style={[styles.text]}>
+              Acessar
+          </Text>
+      </TouchableOpacity>
     );
   }
   render() {
     return (
-      <ImageBackground style={{ flex: 1, width: null }} source={require('../imgs/background.jpg')}>
+      <ImageBackground style={{ flex: 1, width: null }} source={require('../imgs/maxresdefault.jpg')}>
     <View style={{ flex: 1, padding: 10 }} >
 
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text style={{ fontSize: 30, color: '#fff' }}>WhatsApp ?</Text>
+        <Text style={{ fontSize: 30, color: '#fff' }}>ChatZapp</Text>
       </View>
 
-      <View style={{ flex: 2 }}>
+      <View style={{ flex: 2}}>
         <TextInput 
         value={this.props.email} 
-        style={{ fontSize: 20, height: 45, color: '#fff' }} 
+        style={{backgroundColor: '#000',borderRadius: 10, fontSize: 20, height: 50, color: '#fff' }} 
         placeholder='E-Mail' 
         placeholderTextColor='#CEC9C9' 
         onChangeText={texto => this.props.modificaEmail(texto)} 
@@ -45,7 +47,7 @@ class formLogin extends Component {
         <TextInput 
         secureTextEntry 
         value={this.props.senha} 
-        style={{ fontSize: 20, height: 45, color: '#fff' }} 
+        style={{backgroundColor: '#000',marginTop: 5, borderRadius: 10, fontSize: 20, height: 50, color: '#fff' }} 
         placeholder='Senha' 
         placeholderTextColor='#CEC9C9' 
         onChangeText={texto => this.props.modificaSenha(texto)} 
@@ -56,7 +58,7 @@ class formLogin extends Component {
           <TouchableHighlight 
           onPress={() => { Actions.formCadastro(); }}
           > 
-          <View style={{ flexDirection: 'row' }}>
+          <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
             <Text style={{ fontSize: 18, color: '#fff', marginTop: 10 }}>
             Ainda não tem cadastro ?
             </Text>
@@ -65,7 +67,7 @@ class formLogin extends Component {
           </TouchableHighlight> 
       </View>
 
-      <View style={{ flex: 2 }}>
+      <View style={{ flex: 2}}>
         {this.renderBtnAcessar()}
       </View>
     </View>
@@ -73,6 +75,24 @@ class formLogin extends Component {
     );
   }
 }
+const styles = StyleSheet.create({
+  button: {
+      height: 50,
+      borderRadius: 10,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#000',
+      shadowColor: '#fff',
+      shadowOpacity: 0.4,
+      shadowOffset: { height: 10, width: 0 },
+      shadowRadius: 20,
+  },
+  text: {
+      fontSize: 16,
+      textTransform: 'uppercase',
+      color: '#FFFFFF',
+  },
+});
 const mapStateToProps = state => (
   {
   email: state.AutenticacaoReducer.email,
